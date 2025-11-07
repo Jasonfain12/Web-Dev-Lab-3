7eb989b7d16ee646b6c6ba6a3d8c0b7f774f3a23
Jasonfain12 <jason.fain@gtbeta.org>
2025-11-07 15:55:35 -0500

    Add files via upload

---
+++ WebDevelopmentLab03/pages/Graph page.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import json # The 'json' module is needed to work with JSON files.
+import os   # The 'os' module helps with file system operations.
+import matplotlib.pyplot as plt
+# PAGE CONFIGURATION
+st.set_page_config(
+    page_title="Visualizations",
+    page_icon="📈",
+)
+
+# PAGE TITLE AND INFORMATION
+st.title("Exercise Data Visualizations 📈")
+
+
+# DATA LOADING
+# A crucial step is to load the data from the files.
+# It's important to add error handling to prevent the app from crashing if a file is empty or missing.
+
+st.divider()
