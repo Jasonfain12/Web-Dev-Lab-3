0c09bb8551b6d468d0d6c9649fee1c15dcd9854b
danielgalante12 <danielgalante2007@gmail.com>
2025-11-13 13:50:23 -0500

    Update Home_Page.py

--- WebDevelopmentLab03/Home_Page.py
+++ WebDevelopmentLab03/Home_Page.py
@@ -7,16 +7,16 @@ st.title("Web Development Lab03")
 # TODO: Fill out your team number, section, and team members
 
 st.header("CS 1301")
-st.subheader("Team XX, Web Development - Section X")
-st.subheader("Team Member 1, Team Member 2")
+st.subheader("Team 37, Web Development - Section E")
+st.subheader("Daniel Galante, Jason Fain")
 
 
 # Introduction
 # TODO: Write a quick description for all of your pages in this lab below, in the form:
-#       1. **Page Name**: Description
-#       2. **Page Name**: Description
-#       3. **Page Name**: Description
-#       4. **Page Name**: Description
+#       1. **Home Page**: Intro/Title page allowing for navigation to other pages
+#       2. **Character Analysis**: Page providing information and analytics (via graphs) of all characters in the Rick and Morty Series
+#       3. **Character Battler**: Get two characters from the Rick and Morty Series to fight (and see who wins)
+#       4. **Rick and Morty ChatBot**: AI chatbot specializing in all things Rick and Morty
 
 st.write("""
 Welcome to our Streamlit Web Development Lab03 app! You can navigate between the pages using the sidebar to the left. The following pages are:
