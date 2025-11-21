31846ca71e34b44eaa9b88ffe87821ca296f6f9c
Jasonfain12 <jason.fain@gtbeta.org>
2025-11-21 16:19:08 -0500

    Update Rick and Morty AI Chatbot.py

--- WebDevelopmentLab03/pages/Rick and Morty AI Chatbot.py
+++ WebDevelopmentLab03/pages/Rick and Morty AI Chatbot.py
@@ -1,1 +1,6 @@
-print("No Data Found")
+import google.generativeai as genai
+import os
+
+print(genai.__file__)   # Where is this module loaded from?
+print(genai.__version__)  # Confirm version again
+print(hasattr(genai, "Client"))  # Is Client attribute present at all?
