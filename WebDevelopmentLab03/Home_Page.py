9ef716d61d3d7c654d4040c9da18c0390d2cd359
danielgalante12 <danielgalante2007@gmail.com>
2025-11-30 23:00:26 -0500

    Update Home_Page.py

--- WebDevelopmentLab03/Home_Page.py
+++ WebDevelopmentLab03/Home_Page.py
@@ -15,15 +15,14 @@ st.subheader("Daniel Galante, Jason Fain")
 # TODO: Write a quick description for all of your pages in this lab below, in the form:
 #       1. **Home Page**: Intro/Title page allowing for navigation to other pages
 #       2. **Character Analysis**: Page providing information and analytics (via graphs) of all characters in the Rick and Morty Series
-#       3. **Character Battler**: Get two characters from the Rick and Morty Series to fight (and see who wins)
-#       4. **Rick and Morty ChatBot**: AI chatbot specializing in all things Rick and Morty
+#       3. **Guide for Travel Brochure**: Get a travel brochure for any planet you wish to travel to in the Rick and Morty Universe, in the tone you want it to be presented!
+#       4. **Rick and Morty ChatBot**: An AI chatbot specializing in all things Rick and Morty
 
 st.write("""
 Welcome to our Streamlit Web Development Lab03 app! You can navigate between the pages using the sidebar to the left. The following pages are:
 
-1. Home Page
-2. Character Analysis
-3. Guide for Travel Brochure
-4. Rick and Morty AI ChatBot
+1. **Character Analysis**: Information and analytics (via graphs) of all characters in the Rick and Morty Series
+2. **Guide for Travel Brochure**: Get a travel brochure for any planet you wish to travel to in the Rick and Morty Universe, in the tone you want it to be presented!
+3. **Rick and Morty AI ChatBot**: An AI chatbot specializing in all things Rick and Morty
 
 """)
